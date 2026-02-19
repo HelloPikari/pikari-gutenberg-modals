3778cddcec26af768abd2688d33348db86c16299
Steve Ariss <steve@pikari.io>
2026-02-19 15:59:13 -0500

    updated block icons

--- src/blocks/modal-trigger/icon.js
+++ src/blocks/modal-trigger/icon.js
@@ -1,10 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { SVG, Path } from '@wordpress/primitives';
+import { SVG, Path, Rect, Polygon } from '@wordpress/primitives';
 
 export default (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M4 4h16v16H4V4zm2 2v12h12V6H6zm3 3h6v1.5H9V9zm0 3.5h4V14H9v-1.5zM18.5 14l2.5 3h-2v3h-1v-3h-2l2.5-3z" />
+		<Rect x="6.11" y="9.89" width="11.35" height="1.5" />
+		<Path d="M19.25,21H4.75c-.97,0-1.75-.79-1.75-1.75V5.75c0-.97.79-1.75,1.75-1.75h7.35v1.5h-7.35c-.14,0-.25.11-.25.25v13.49c0,.14.11.25.25.25h14.49c.14,0,.25-.11.25-.25v-7.23h1.5v7.23c0,.97-.79,1.75-1.75,1.75Z" />
+		<Rect x="6.11" y="16" width="6.38" height="1.5" />
+		<Rect x="6.11" y="13" width="11.35" height="1.5" />
+		<Polygon points="15.69 3 15.69 4.5 18.34 4.5 15.38 7.47 16.44 8.53 19.5 5.46 19.5 8.02 21 8.02 21 3 15.69 3" />
 	</SVG>
 );
