dd74533457d9186216590a14035e3b3fa3339817
Steve Ariss <steve@pikari.io>
2026-02-23 11:12:46 -0500

    updated version number

--- pikari-gutenberg-modals.php
+++ pikari-gutenberg-modals.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pikari Gutenberg Modals
  * Plugin URI:  https://pikari.io
  * Description: Modal windows for the WordPress Gutenberg block editor. Adds accessible modal dialogs
- * Version:     1.2.1
+ * Version:     1.2.2
  * Author:      Pikari Inc.
  * Author URI:  https://pikari.io
  * License:     GPL-2.0-or-later
