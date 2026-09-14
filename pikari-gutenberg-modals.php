d8b7c876d01f499e3c9e8196ebbe70a4162adfba
HelloPikari <87791563+HelloPikari@users.noreply.github.com>
2026-09-14 07:07:44 +0000

    chore: Bump version to 2.2.3

--- pikari-gutenberg-modals.php
+++ pikari-gutenberg-modals.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pikari Gutenberg Modals
  * Plugin URI:  https://pikari.io
  * Description: Modal windows for the WordPress Gutenberg block editor. Adds accessible modal dialogs
- * Version:     2.2.2
+ * Version:     2.2.3
  * Author:      Pikari Inc.
  * Author URI:  https://pikari.io
  * License:     GPL-2.0-or-later
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-define( 'PIKARI_GUTENBERG_MODALS_VERSION', '2.2.2' );
+define( 'PIKARI_GUTENBERG_MODALS_VERSION', '2.2.3' );
 
 /**
  * Plugin directory path.
